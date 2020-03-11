ea271ed15485f7a49bd02b88bbe3f6f5767068bc
Brett Jepsen <brett.jepsen@aggiemail.usu.edu>
2020-03-10 19:00:34 -0600

    install script for stamina

---
+++ install.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use Cwd qw(cwd);
+
+# First we need to clone prism
+my $originalCwd = cwd;
+installPrism($originalCwd);
+installStamina($originalCwd);
+setEnvVariable();
+
+sub installPrism($originalCwd) {
+    my @command = (
+        "cd $originalCwd && rm -rf prism && git clone https://github.com/prismmodelchecker/prism prism && cd prism/prism && git checkout v4.5 && make -j4",
+    );
+    system(shift @command);
+}
+
+sub installStamina() {
+    my $dir = cwd;
+    my @command = (
+        "cd $originalCwd && rm -rf stamina && git clone https://github.com/fluentverification/stamina.git && cd stamina/stamina && make -j4 PRISM_HOME=$dir/prism/prism"
+    );
+    system(shift @command);
+}
+
+sub setEnvVariable() {
+    my @command = (
+        "export _JAVA_OPTIONS=-Xmx65536m"
+    );
+    system(shift @command);
+}
